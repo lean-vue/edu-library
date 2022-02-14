c5b2e06cb9fbc752b908d4f4ee8b8c51ef2a53b9
Micha Alt <mail@lean-stack.de>
2022-02-14 23:42:24 +0100

    Link the library project and use the button.

--- apps/sink/src/App.vue
+++ apps/sink/src/App.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { VuiButton } from '@magro/vui';
+</script>
 
 <template>
   <main>
     <h1>Vui Sink</h1>
+    <div>
+      <VuiButton label="Click me!" />
+    </div>
   </main>
 </template>
