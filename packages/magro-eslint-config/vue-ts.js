56e72be5dbbf568b7964c60e29f2d18f74bfa9e8
Micha Alt <mail@lean-stack.de>
2022-02-17 21:53:32 +0100

    Create a shareable eslint config package.

---
+++ packages/magro-eslint-config/vue-ts.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    'vue/setup-compiler-macros': true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+};
