cbd2726969bbc8e174e4756b999e68d50491ad69
Micha Alt <mail@lean-stack.de>
2022-02-20 15:26:20 +0100

    Style samples starter.

--- samples/sample-template/src/App.vue
+++ samples/sample-template/src/App.vue
@@ -1,43 +1,27 @@
 <script setup lang="ts"></script>
 
 <template>
-  <header class="page">
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" />
-    <h1>Lean Vue Samples</h1>
+  <header
+    class="bg-white border-b border-gray-200 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"
+  >
+    <div class="h-16 flex items-center">
+      <img alt="Vue logo" class="w-12 h-12" src="./assets/logo.svg" />
+      <h1 class="text-3xl font-semibold ml-4">Lean Vue Samples</h1>
+    </div>
   </header>
 
-  <main>
-    <article>
-      <header>
-        <h2>Samples Starter Template</h2>
-        <p>Ausgangspunkt meiner Beispiele.</p>
+  <main class="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 mt-8">
+    <article
+      class="bg-white border border-gray-200 shadow-sm overflow-hidden rounded-lg"
+    >
+      <header class="px-4 py-5 border-b border-gray-200 sm:px-6">
+        <h2 class="text-lg leading-6 font-medium text-gray-900">
+          Samples Starter Template
+        </h2>
       </header>
+      <div class="px-4 py-5 sm:p-6">
+        <p>Ausgangspunkt meiner Beispiele.</p>
+      </div>
     </article>
   </main>
 </template>
-
-<style scoped>
-header.page {
-  display: flex;
-  gap: 1rem;
-  padding: 0 2rem;
-  align-items: center;
-  height: 4rem;
-  border-bottom: 1px solid #818181;
-}
-header.page img {
-  height: 40px;
-}
-
-h1,
-h2 {
-  font-weight: 500;
-}
-
-main {
-  color: #313131;
-  margin: 2rem auto 0;
-  padding: 0 1rem;
-  max-width: 720px;
-}
-</style>
